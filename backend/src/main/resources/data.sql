2edf1ae3e831ac8c2cf0e91927b4dca172e6e3dc
Marco Braga <maboa17@gmail.com>
2022-02-23 18:20:01 -0300

    data.sql fixed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -5,7 +5,6 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 
